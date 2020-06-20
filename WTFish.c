7a1db6f5a2efd3727f07d0d2ce05d2da5ded941b
Sudipta Kundu <sdptknd@gmail.com>
2020-06-21 03:33:30 +0530

    added functionality that can replace home path with ~
    also added color in directory showing

--- WTFish.c
+++ WTFish.c
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+typedef enum
+{
+  false,
+  true
+} Bool;
+
 void remove_new_line(char *string)
 {
   for (unsigned index = 0; string[index] != '\0'; index++)
@@ -14,15 +20,27 @@ void remove_new_line(char *string)
   }
 }
 
+char *replace_home_path(char *home, char *cwd)
+{
+  unsigned home_path_length = strlen(home);
+  Bool is_home_path_present = strncmp(home, cwd, home_path_length) == 0;
+  if (!is_home_path_present)
+    return cwd;
+
+  cwd[home_path_length - 1] = '~';
+  return cwd + home_path_length - 1;
+}
+
 int main(void)
 {
   char command_string[50];
   char cwd[50];
+  char *home = getenv("HOME");
 
   while (1)
   {
     getcwd(cwd, sizeof(cwd));
-    printf("\nWTFish(%s)-> ", cwd);
+    printf("\nWTFish (\033[0;36m%s\033[0m)-> ", replace_home_path(home, cwd));
     fgets(command_string, 50, stdin);
     remove_new_line(command_string);
 
